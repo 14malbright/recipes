783a85fd93ca2f370998dc76a38937a0d82d973f
mason <14malbright@gmail.com>
2020-01-06 04:21:22 -0500

    Add cache.py

---
+++ cache.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+from functools import wraps
+
+
+def default_key(*args, **kwargs):
+    return (tuple(args) + tuple(sorted(kwargs.items())))
+
+
+def Cache(base=dict, *, name=None, key_func=None):  # XXX: check function defs
+    if name is None:
+        name = base.__name__ + "Cache"
+
+    if key_func is None:
+        key_func = default_key
+
+    def init(self, func):  # XXX: use mixin instead?
+        wraps(func)(self)
+        base.__init__(self)
+
+    def call(self, *args, **kwargs):
+        key = key_func(*args, **kwargs)
+        if key not in self:
+            result = self.__wrapped__(*args, **kwargs)
+            self[key] = result
+        else:
+            result = self[key]
+        return result  # indirection for weakref
+
+    return type(name, (base,), {"__init__": init,
+                                "__call__": call})
+
+
+Cache = Cache()(Cache)  # remember created classes (e.g., dictCache)
+
+
+if __name__ == "__main__":
+    from weakref import WeakValueDictionary
+
+    @Cache()
+    def add(x, y):
+        """Add x and y."""
+        return x + y
+
+    @Cache()
+    def sub(x, y):
+        return x - y
+
+    class Test(object):
+        def __init__(self, x, y):
+            self.out = x * y
+
+        @Cache()  # does not work for methods
+        def div(self, a, b):
+            return a / b
+
+        def __repr__(self):
+            return str(self.out)
+
+    @Cache(WeakValueDictionary)
+    def mul(x, y):
+        return Test(x, y)
